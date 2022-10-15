38598705d96ba1702ecf4fe0e0a84f0af99927f3
N <115864453+xvmwot@users.noreply.github.com>
2022-10-15 10:05:32 +0300

    python start

---
+++ urok1Nik.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import *
+
+def test(argument):
+    print(argument)
+
+test("ян пукнул под себя пуки-пуки-пуки")
+
+list = []
+list.append("под себя сильно пукнул калпачки ворует с машини")
+print(list)
+mnoj = {}
+print(f"Spisok skolko raz yan pyknyl {list}")
